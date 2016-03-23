08b7e8de27582fc10a082877492766e128dfb2cd
mizuhiki <tmizuhiki@gmail.com>
2016-03-23 16:10:42 +0900

    Fixes build errors and crashes under in Xcode 7 build

--- JuliusSample/Classes/JuliusSampleAppDelegate.m
+++ JuliusSample/Classes/JuliusSampleAppDelegate.m
@@ -24,6 +24,8 @@
 
     // Add the view controller's view to the window and display.
     [self.window addSubview:viewController.view];
+    self.window.rootViewController = viewController;
+    
     [self.window makeKeyAndVisible];
 
     return YES;
